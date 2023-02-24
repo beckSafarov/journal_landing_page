19aa4a0ff2654b998e2b98ecccb5baba93814523
Begzod Safarov <begzod.safarov07@gmail.com>
2023-02-24 20:53:00 +0500

    plannedconferences made mobile responsive

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
         <SearchSection />
         <Journals />
         <Stats />
+        <PlannedConferencesSection />
         {(isDesktop || isTablet) && (
           <>
-            <PlannedConferencesSection />
             <OrganizationsSection />
             <OurAddressSection />
             <Footer />
